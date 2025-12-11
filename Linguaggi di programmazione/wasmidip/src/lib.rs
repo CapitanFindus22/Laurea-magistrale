b4f26e92e1b8bc4dbd22d98bb82b2292e6eb146f
leonardo <ganzaroli.leonardo@gmail.com>
2025-12-11 16:49:21 +0100

    work with JS now

--- Linguaggi di programmazione/wasmidip/src/lib.rs
+++ Linguaggi di programmazione/wasmidip/src/lib.rs
@@ -1,14 +1,48 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use wasm_bindgen::prelude::*;   //JS <-> WASM
+use std::sync::Arc;             //Serve per usare rustysynth
+use rustysynth::{
+    SoundFont,
+    Synthesizer,
+    SynthesizerSettings,
+    MidiFile,
+    MidiFileSequencer
+};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[wasm_bindgen] //Esportabile in JS
+pub fn render_midi(sf2_data: &[u8], midi_data: &[u8]) -> Vec<f32> {
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    console_error_panic_hook::set_once();
+
+    // Carica soundfont
+    let mut sf2_cursor = std::io::Cursor::new(sf2_data);
+    let sound_font = Arc::new(SoundFont::new(&mut sf2_cursor).unwrap());
+
+    // Carica MIDI
+    let mut midi_cursor = std::io::Cursor::new(midi_data);
+    let midi_file = Arc::new(MidiFile::new(&mut midi_cursor).unwrap());
+
+    // Crea sintetizzatore
+    let settings = SynthesizerSettings::new(44100);
+    let synth = Synthesizer::new(&sound_font, &settings).unwrap();
+
+    // Crea sequenziatore
+    let mut sequencer = MidiFileSequencer::new(synth);
+    sequencer.play(&midi_file, false);
+
+    // Calcola samples totali
+    let sample_count = (settings.sample_rate as f64 * midi_file.get_length()) as usize;
+
+    // Buffers audio sx e dx 
+    let mut left = vec![0.0f32; sample_count];
+    let mut right = vec![0.0f32; sample_count];
+    sequencer.render(&mut left, &mut right);
+
+    // Valori interleaved per semplificare estrazione successivamente
+    let mut out = Vec::with_capacity(sample_count * 2);
+    for i in 0..sample_count {
+        out.push(left[i]);
+        out.push(right[i]);
     }
+
+    out
 }
